37f2653e4af36531b8327ba29a089f344802f301
chyngyz <chyngyz.omurov@gmail.com>
2021-09-14 11:32:22 +0600

    Create empty market-kit project structure

---
+++ app/src/main/java/io/horizontalsystems/marketkit/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.horizontalsystems.marketkit.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.horizontalsystems.marketkit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/MarketKit.kt
@@ -0,0 +1,5 @@
+package io.horizontalsystems.marketkit
+
+class MarketKit {
+
+}
