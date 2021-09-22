40fb5f27b06894db2111e25c3c9d9d91e3193054
chyngyz <chyngyz.omurov@gmail.com>
2021-09-22 13:23:07 +0600

    Remove redundant methods from Platform

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/Platform.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/Platform.kt
@@ -1,7 +1,6 @@
 package io.horizontalsystems.marketkit.models
 
 import androidx.room.Entity
-import java.util.*
 
 @Entity(primaryKeys = ["coinType", "coinUid"])
 data class Platform(
@@ -11,17 +10,11 @@ data class Platform(
 ) {
     companion object {
         fun getInstance(platformResponse: PlatformResponse, coinUid: String): Platform? {
-            val coinType = CoinType.getInstance(platformResponse.type, platformResponse.address, platformResponse.symbol) ?: return null
+            val coinType =
+                CoinType.getInstance(platformResponse.type, platformResponse.address, platformResponse.symbol)
+                    ?: return null
             return Platform(coinType, platformResponse.decimal, coinUid)
         }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is Platform && other.coinType == coinType && other.decimal == decimal && other.coinUid == coinUid
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(coinType, decimal, coinUid)
     }
 
     override fun toString(): String {
