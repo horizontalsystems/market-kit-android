3be03e1567a331131a39fd79c3fab7b88b5acdd1
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-03-04 17:08:29 +0600

    Add new tvl change periods to DefiMarketInfo

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/DefiMarketInfoResponse.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/DefiMarketInfoResponse.kt
@@ -13,10 +13,18 @@ data class DefiMarketInfoResponse(
     val tvlRank: Int,
     @SerializedName("tvl_change_1d")
     val tvlChange1D: BigDecimal?,
-    @SerializedName("tvl_change_7d")
-    val tvlChange7D: BigDecimal?,
-    @SerializedName("tvl_change_30d")
-    val tvlChange30D: BigDecimal?,
+    @SerializedName("tvl_change_1w")
+    val tvlChange1W: BigDecimal?,
+    @SerializedName("tvl_change_2w")
+    val tvlChange2W: BigDecimal?,
+    @SerializedName("tvl_change_1m")
+    val tvlChange1M: BigDecimal?,
+    @SerializedName("tvl_change_3m")
+    val tvlChange3M: BigDecimal?,
+    @SerializedName("tvl_change_6m")
+    val tvlChange6M: BigDecimal?,
+    @SerializedName("tvl_change_1y")
+    val tvlChange1Y: BigDecimal?,
     val chains: List<String>,
     @SerializedName("chain_tvls")
     val chainTvls: Map<String, BigDecimal?>,
