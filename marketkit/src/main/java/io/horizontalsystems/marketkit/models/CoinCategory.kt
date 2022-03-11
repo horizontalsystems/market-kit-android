7ad4278775e2adc6530558ce0c1f12101c3c1550
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-03-11 17:59:43 +0600

    Make CoinCategory Parcelable

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/CoinCategory.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/CoinCategory.kt
@@ -1,8 +1,11 @@
 package io.horizontalsystems.marketkit.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class CoinCategory(
     @PrimaryKey
@@ -10,7 +13,7 @@ data class CoinCategory(
     val name: String,
     val description: Map<String, String>,
     val order: Int
-) {
+) : Parcelable {
     override fun toString(): String {
         return "CoinCategory [uid: $uid; name: $name; descriptionCount: ${description.size}; order: $order]"
     }
