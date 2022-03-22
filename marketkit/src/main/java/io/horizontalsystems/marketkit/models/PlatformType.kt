f471d7f493047b1a44956d6ea04901286d2888bd
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-03-22 15:59:38 +0600

    Add Optimism and ArbitrumOne platform types

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/PlatformType.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/PlatformType.kt
@@ -3,12 +3,16 @@ package io.horizontalsystems.marketkit.models
 enum class PlatformType {
     Ethereum,
     BinanceSmartChain,
-    Polygon;
+    Polygon,
+    Optimism,
+    ArbitrumOne;
 
     val coinTypeIdPrefixes: List<String>
         get() = when (this) {
             Ethereum -> listOf("ethereum", "erc20")
             BinanceSmartChain -> listOf("binanceSmartChain", "bep20")
             Polygon -> listOf("polygon", "mrc20")
+            Optimism -> listOf("ethereumOptimism", "optimismErc20")
+            ArbitrumOne -> listOf("ethereumArbitrumOne", "arbitrumOneErc20")
         }
 }
