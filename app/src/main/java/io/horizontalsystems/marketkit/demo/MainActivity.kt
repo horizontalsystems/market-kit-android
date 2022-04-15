81609c90c8ed542074d237b62fe2c308d0f12781
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-04-15 14:55:40 +0600

    Update libraries to latest
    - Set Java to 11

--- app/src/main/java/io/horizontalsystems/marketkit/demo/MainActivity.kt
+++ app/src/main/java/io/horizontalsystems/marketkit/demo/MainActivity.kt
@@ -3,59 +3,60 @@ package io.horizontalsystems.marketkit.demo
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.marketkit.MarketKit
-import io.horizontalsystems.marketkit.R
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.activity_main.*
+import io.horizontalsystems.marketkit.databinding.ActivityMainBinding
+import io.horizontalsystems.marketkit.databinding.ViewHolderItemBinding
 import kotlin.reflect.KFunction
 import kotlin.reflect.full.memberFunctions
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel = ViewModelProvider(this, ViewModelFactory(this)).get(MainViewModel::class.java)
-
 
         val methods = MainViewModel::class.memberFunctions.filter {
             it.name.startsWith("run")
         }
 
         val adapter = Adapter(methods, viewModel)
-        rv.adapter = adapter
+        binding.rv.adapter = adapter
     }
 
 }
 
-class Adapter(private val methods: List<KFunction<*>>, private val viewModel: MainViewModel) : RecyclerView.Adapter<Adapter.VH>() {
+class Adapter(private val methods: List<KFunction<*>>, private val viewModel: MainViewModel) :
+    RecyclerView.Adapter<Adapter.VH>() {
 
-    class VH(override val containerView: View, private val viewModel: MainViewModel) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-
-        private val button = containerView.findViewById<TextView>(R.id.button)
+    class VH(private val binding: ViewHolderItemBinding, private val viewModel: MainViewModel) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(method: KFunction<*>) {
-            button.text = method.name.substringAfter("run")
-            button.setOnClickListener {
+            binding.button.text = method.name.substringAfter("run")
+            binding.button.setOnClickListener {
                 method.call(viewModel)
             }
         }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        return VH(LayoutInflater.from(parent.context).inflate(R.layout.view_holder_item, parent, false), viewModel)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        VH(
+            ViewHolderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            viewModel
+        )
 
     override fun onBindViewHolder(holder: VH, position: Int) {
         holder.bind(methods[position])
@@ -76,7 +77,7 @@ class ViewModelFactory(context: Context) : ViewModelProvider.Factory {
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainViewModel(marketKit) as T
     }
 }
