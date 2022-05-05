4d75a7120d8d842bf22906ed247a13bdf881a4e7
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-05-05 14:32:36 +0600

    Update topPlatform API response
    - Pass currencyCode as parameter

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/TopPlatform.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/TopPlatform.kt
@@ -3,14 +3,15 @@ package io.horizontalsystems.marketkit.models
 import java.math.BigDecimal
 
 data class TopPlatform(
-    val coin: FullCoin,
-    val marketCap: BigDecimal,
+    val uid: String,
+    val name: String,
     val rank: Int,
+    val protocols: Int,
+    val marketCap: BigDecimal,
     val rank1D: Int?,
     val rank1W: Int?,
     val rank1M: Int?,
     val change1D: BigDecimal?,
     val change1W: BigDecimal?,
     val change1M: BigDecimal?,
-    val protocols: Int?
 )
