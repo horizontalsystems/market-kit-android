2a92510674c80331b168e5cae6b410c7bbc8f747
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-05-23 16:22:03 +0600

    Rename parameter annotation to fix data parsing

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/CoinCategoryMarketPoint.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/CoinCategoryMarketPoint.kt
@@ -4,7 +4,6 @@ import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
 
 data class CoinCategoryMarketPoint(
-    @SerializedName("date")
     val timestamp: Long,
     @SerializedName("market_cap")
     val marketCap: BigDecimal,
