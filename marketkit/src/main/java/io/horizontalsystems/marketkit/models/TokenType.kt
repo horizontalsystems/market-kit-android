320abe6db24c2bae49e4f9e93f5ecdac2163c9f9
abdrasulov <abdrasulov@gmail.com>
2025-04-24 16:40:35 +0600

    Add TokenType for Stellar Asset

--- marketkit/src/main/java/io/horizontalsystems/marketkit/models/TokenType.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/models/TokenType.kt
@@ -36,6 +36,9 @@ sealed class TokenType : Parcelable {
     data class Jetton(val address: String) : TokenType()
 
     @Parcelize
+    data class Asset(val code: String, val issuer: String) : TokenType()
+
+    @Parcelize
     data class Unsupported(val type: String, val reference: String) : TokenType()
 
     val id: String
@@ -45,6 +48,7 @@ sealed class TokenType : Parcelable {
                 is Eip20 -> listOf("eip20", address)
                 is Spl -> listOf("spl", address)
                 is Jetton -> listOf("the-open-network", address)
+                is Asset -> listOf("stellar", "$code-$issuer")
                 is AddressTyped -> listOf("address_type", type.name.lowercase())
                 is Derived -> listOf("derived", derivation.name.lowercase())
                 is Unsupported -> if (reference.isNotBlank()) {
@@ -62,6 +66,7 @@ sealed class TokenType : Parcelable {
             is Eip20 -> Value("eip20", address)
             is Spl -> Value("spl", address)
             is Jetton -> Value("the-open-network", address)
+            is Asset -> Value("stellar", "$code-$issuer")
             is AddressTyped -> Value("address_type", type.name)
             is Derived -> Value("derived", derivation.name)
             is Unsupported -> Value(type, reference)
@@ -93,6 +98,13 @@ sealed class TokenType : Parcelable {
                 "the-open-network" -> {
                     if (reference.isNotBlank()) {
                         return Jetton(reference)
+                    }
+                }
+
+                "stellar" -> {
+                    val parts = reference.split("-")
+                    if (parts.size == 2) {
+                        return Asset(parts[0], parts[1])
                     }
                 }
 
