55f85854f874ee6384086ee1b27ab945d0fc7be4
chyngyz <chyngyz.omurov@gmail.com>
2025-06-13 17:03:46 +0600

    Escape values when creating coin list dump

--- marketkit/src/main/java/io/horizontalsystems/marketkit/managers/DumpManager.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/managers/DumpManager.kt
@@ -11,27 +11,25 @@ class DumpManager(private val marketDatabase: MarketDatabase) {
                 "CREATE TABLE IF NOT EXISTS `TokenEntity` (`coinUid` TEXT NOT NULL, `blockchainUid` TEXT NOT NULL, `type` TEXT NOT NULL, `decimals` INTEGER, `reference` TEXT NOT NULL, PRIMARY KEY(`coinUid`, `blockchainUid`, `type`, `reference`), FOREIGN KEY(`coinUid`) REFERENCES `Coin`(`uid`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`blockchainUid`) REFERENCES `BlockchainEntity`(`uid`) ON UPDATE NO ACTION ON DELETE CASCADE )\n"
 
     fun getInitialDump(): String {
+        val esc: (String?) -> String = { it?.let(DatabaseUtils::sqlEscapeString) ?: "null" }
         val insertQueries = StringBuilder()
         insertQueries.append(tablesCreation)
         val blockchains = marketDatabase.blockchainEntityDao().getAll()
         blockchains.forEach { blockchain ->
-            val eipUrl = blockchain.eip3091url?.let { "'$it'" } ?: "null"
             val insertQuery =
-                "INSERT INTO BlockchainEntity VALUES('${blockchain.uid}',${DatabaseUtils.sqlEscapeString(blockchain.name)},$eipUrl);"
+                "INSERT INTO BlockchainEntity VALUES(${esc(blockchain.uid)},${esc(blockchain.name)},${esc(blockchain.eip3091url)});"
             insertQueries.append(insertQuery).append("\n")
         }
         val tokens = marketDatabase.tokenEntityDao().getAll()
         tokens.forEach { token ->
-            val reference = token.reference?.let { "'$it'" } ?: "null"
             val insertQuery =
-                "INSERT INTO TokenEntity VALUES('${token.coinUid}','${token.blockchainUid}','${token.type}',${token.decimals},$reference);"
+                "INSERT INTO TokenEntity VALUES(${esc(token.coinUid)},${esc(token.blockchainUid)},${esc(token.type)},${token.decimals},${esc(token.reference)});"
             insertQueries.append(insertQuery).append("\n")
         }
         val coins = marketDatabase.coinDao().getAllCoins()
         coins.forEach { coin ->
-            val coinGeckoId = coin.coinGeckoId?.let { "'$it'" } ?: "null"
             val insertQuery =
-                "INSERT INTO Coin VALUES('${coin.uid}',${DatabaseUtils.sqlEscapeString(coin.name)},'${coin.code}',${coin.marketCapRank},$coinGeckoId,'${coin.image}');"
+                "INSERT INTO Coin VALUES(${esc(coin.uid)},${esc(coin.name)},${esc(coin.code)},${coin.marketCapRank},${esc(coin.coinGeckoId)},${esc(coin.image)});"
             insertQueries.append(insertQuery).append("\n")
         }
         return insertQueries.toString()
